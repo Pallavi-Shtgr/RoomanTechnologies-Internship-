158475a3c9147ce77af86a163977d011fce3c24a
Pallavi <pallavishettigar03@gmail.com>
2024-09-26 21:19:56 +0530

    Add the code

---
+++ Code test 1.py
@@ -0,0 +1,11 @@
+# Write a Python function greet_user(name) that accepts the user's name as a parameter and prints a greeting message.
+
+# Input:
+# The function will accept a single parameter name which is a string.
+
+# Output:
+# The function should print "Hello, <name>!" where <name> is the value passed as the parameter.
+
+def greet_user(name):
+    print(f"Hello, {name}!")   
+greet_user("Pallavi ")
