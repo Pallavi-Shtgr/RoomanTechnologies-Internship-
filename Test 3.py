38cc6defa33ab379c22ab6bc89a5b33f52f76456
Pallavi <pallavishettigar03@gmail.com>
2024-09-28 22:47:33 +0530

    add changes

---
+++ Test 3.py
@@ -0,0 +1,12 @@
+# Write a Python function add_numbers(num1, num2) that takes two integers as input and returns their sum.
+
+# Input:
+# The function takes two integers num1 and num2.
+
+# Output:
+# The function returns the sum of num1 and num2.
+
+def add_num(num1, num2):
+    return num1 + num2
+
+print("The sum is:", add_num(5, 3))
