33a7f2ddfb68114684e24ca51676b03e6ad286c8
Pallavi <pallavishettigar03@gmail.com>
2024-09-29 21:08:31 +0530

    add changes

--- Test 11.py
+++ Test 11.py
@@ -1,1 +1,13 @@
 """Day 03"""
+
+# Write a Python function is in set(my_set, item) that takes a set my set and a variable item. The function should return True if the item exists in the set, otherwise return False.
+
+# Constraint
+
+# The set will contain up to 10 elements.
+
+def is_in_set(my_set,item):
+    return item in my_set
+test_set={1,2,3,4,5}
+test_item=4
+print(is_in_set(test_set,test_item))
--- Test 9.py
+++ Test 9.py
@@ -1,4 +1,5 @@
 """Day 03"""
+
 # Write a Python function calculate_area(length, width) that takes two inputs: two integers length and width, and returns the area of a rectangle.
 # Constraint
 # Both length and width will be positive integers between 1 and 1000.
