9617f3737317e69469b46e4c09d49952da946b1a
Pallavi <pallavishettigar03@gmail.com>
2024-10-07 21:43:19 +0530

    ADd changes

--- Test 13.py
+++ Test 13.py
@@ -1,16 +1,11 @@
-
-
-
-
-
-
-
-
-
-
+"""Day 04 DATA STRUCTURES"""
 
 # • The current function overwrites the input parameter by redefining 'colors' as a set within the function. Remove this redefinition to use the input parameter.
 # • The input parameter 'colors' should be treated as a tuple, not a set. Ensure you are accessing the tuple using index positions.
 # • To return the second color, access the element at index 1 of the tuple.
 # • Ensure that the function returns a string, which is the color at the specified index.
 # • Test the function with provided test cases to verify the correct implementation.
+
+def get_Second_color(colors):
+    second_colors=colors[1]
+    return second_colors
