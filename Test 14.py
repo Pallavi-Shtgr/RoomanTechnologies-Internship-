3f4adfb1bb79af1c8efd2f979bc9fab2ef5c3277
Pallavi <pallavishettigar03@gmail.com>
2024-10-07 21:50:06 +0530

    Add changes

--- Test 14.py
+++ Test 14.py
@@ -1,2 +1,14 @@
 """Day 04 DATA STRUCTURES"""
 
+# Write a Python function get_capital(capitals, country) that takes a dictionary capitals and a string country, and returns the capital of that country.
+
+# Constraint
+# The dictionary will contain up to 5 country-capital pairs.
+# The country input will be a string with a length between 1 and 30.
+
+def get_capital(country,capitals):
+
+    if country in capitals:
+        return capitals[country]
+    else:
+        return "Capital not found"
