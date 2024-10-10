6ff432c860a52dd0f564cb7707084de45feb01c5
Pallavi <pallavishettigar03@gmail.com>
2024-10-10 21:46:32 +0530

    add changes

--- Day 05/Test 19.py
+++ Day 05/Test 19.py
@@ -4,3 +4,18 @@
 # Add 10 to each element in the array.
 # Calculate and return the mean of the original array.
 # The function should return a dictionary with the modified array and the mean.
+
+import numpy as np
+def numpy_operations()-> dict:
+   original_array= np.array([1,2,3,4,5])
+
+   modified_array=original_array+10
+
+   mean_value=np.mean(original_array)
+
+   return{
+    'modified_array':modified_array,
+    'mean':mean_value
+   }
+    
+
