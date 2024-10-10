b1dad11b81ac7bc3914a2dc47d6253e194fa9f58
Pallavi <pallavishettigar03@gmail.com>
2024-10-10 21:26:40 +0530

    add changes

--- Day 05/Test 16.py
+++ Day 05/Test 16.py
@@ -1,1 +1,8 @@
 """Day 05 MODULES AND LIBRARIES """
+
+
+# Write a Python function math_operations(num: int) -> dict that takes a user input  integer num and returns a dictionary containing:
+# The square root of num.
+# The sine of 90 degrees.
+# The factorial of num.
+
