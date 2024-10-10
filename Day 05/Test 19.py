067971e23fb7e565c32949fd32f4eb28b6ea80c3
Pallavi <pallavishettigar03@gmail.com>
2024-10-10 21:44:42 +0530

    add changes

--- Day 05/Test 19.py
+++ Day 05/Test 19.py
@@ -1,1 +1,6 @@
 """Day 05 MODULES AND LIBRARIES """
+
+# Create a NumPy array [1, 2, 3, 4, 5].
+# Add 10 to each element in the array.
+# Calculate and return the mean of the original array.
+# The function should return a dictionary with the modified array and the mean.
