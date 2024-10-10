b8afbafcc34de3359daa9d32118b9ebf55924a88
Pallavi <pallavishettigar03@gmail.com>
2024-10-10 21:27:25 +0530

    add changes

--- Day 05/Test 16.py
+++ Day 05/Test 16.py
@@ -6,3 +6,13 @@
 # The sine of 90 degrees.
 # The factorial of num.
 
+import math
+
+def math_operations(num: int) -> dict:
+    result = {}
+    result['square_root'] = math.sqrt(num)
+    result['sine_90_degrees'] = math.sin(math.radians(90))
+    result['factorial'] = math.factorial(num)
+    
+    return result
+
