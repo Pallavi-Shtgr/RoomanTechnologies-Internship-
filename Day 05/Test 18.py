0db81f5fc960a025ffaa2b22124f12c9786bc309
Pallavi <pallavishettigar03@gmail.com>
2024-10-10 21:36:15 +0530

    Add cchanges

--- Day 05/Test 18.py
+++ Day 05/Test 18.py
@@ -1,1 +1,6 @@
 """Day 05 MODULES AND LIBRARIES """
+
+# Write a Python function file_system_operations() -> dict that performs the following operations using the os module and returns a dictionary with:
+# The current working directory.
+# A list of files in the current directory.
+# A confirmation message after creating a new directory called 'new_folder'.
