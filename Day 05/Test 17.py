b40152a8d3d8fefd9605943b2fdbd043b7e14a4b
Pallavi <pallavishettigar03@gmail.com>
2024-10-10 21:28:31 +0530

    add changes

--- Day 05/Test 17.py
+++ Day 05/Test 17.py
@@ -1,1 +1,18 @@
 """Day 05 MODULES AND LIBRARIES """
+
+# Write a Python function get_current_datetime() -> dict that returns a dictionary containing:
+# The current date.
+# The current time.
+# The current date and time formatted as YYYY-MM-DD HH:MM:SS.
+
+import datetime
+
+def get_current_datetime() -> dict:
+    result = {}
+    now = datetime.datetime.now()
+
+    result['current_date'] = now.date()
+    result['current_time'] = now.time()
+
+    result['formatted_datetime'] = now.strftime('%Y-%m-%d %H:%M:%S')
+    return result
