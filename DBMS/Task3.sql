652de6cd47493ad366fc875211f16d316618a318
Pallavi <pallavishettigar03@gmail.com>
2024-12-01 11:25:13 +0530

    Add changes

---
+++ DBMS/Task3.sql
@@ -0,0 +1,2 @@
+-- Create a collection called Customers in a NoSQL database. Insert three customer documents into the Customers collection with the following details: Rahul Sharma, 35, Purchases: ["Laptop", "Phone"] Vikram Seth, 28, Purchases: ["Tablet", "Headphones"] Anjali Gowda, 40, Purchases: ["Camera", "Tripod"] 
+
